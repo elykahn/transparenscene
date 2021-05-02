f3270079f8ece2c1d73e7c99fbe49f06ecb95cf0
Stackbit Code Editor <projects@stackbit.com>
2021-05-02 11:33:29 +0000

    post-list.js: changed

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -51,7 +51,7 @@ const PostListItem = ({
       <PostListMeta>
         <span>{date}</span>
 
-        <span>{timeToRead} mins</span>
+        <span>{timeToRead} m</span>
       </PostListMeta>
     </StyledPostListItem>
   );
