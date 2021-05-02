15d209ff8f67e474d251d565b79703997debdaf8
Stackbit Code Editor <projects@stackbit.com>
2021-05-02 11:29:40 +0000

    index-template.js: changed

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -80,7 +80,7 @@ export const pageQuery = graphql`
         excerpt
         timeToRead
         frontmatter {
-          date(formatString: "MMDD.YYYY")
+          date(formatString: "MM.DD.YYYY")
           description
           title
           tags
