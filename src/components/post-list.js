f891dea8d67840787554c79fb24af05041e83868
Stackbit Code Editor <projects@stackbit.com>
2021-05-14 13:57:12 +0000

    post-list.js: changed

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -77,7 +77,7 @@ const StyledPostListItem = styled.li`
   display: flex;
   flex-direction: column;
   padding: 1.5rem;
-  border: 1px solid rgba(255, 235, 235, 0.5);
+  border: 1px solid rgba(235, 235, 235, 0.5);
   background-color: rgba(255, 255, 235, 0.3);
   backdrop-filter: blur(10px);
   border-radius: 8px;
